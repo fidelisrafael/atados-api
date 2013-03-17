451667a4c62c363972758036a2db8d52a5b275df
yokomizor <mail@rogerioyokomizo.com.br>
2013-03-17 12:11:27 -0300

    added some tests

--- atados/nonprofit/tests/api.py
+++ atados/nonprofit/tests/api.py
@@ -4,8 +4,23 @@ from django.core.urlresolvers import reverse
 
 class RegistrationTest(TestCase):
 
-    def test_show_registration_form(self):
+    def test_show_sign_up_form(self):
         response = self.client.get(reverse('nonprofit:sign-up'))
         self.assertEqual(200, response.status_code)
         self.assertTemplateUsed(response, 'atados/nonprofit/sign-up.html')
 
+    def test_registration_post(self):
+        response = self.client.post(reverse('nonprofit:sign-up'), {
+            'nonprofit_name': 'Atados Test', 'slug': 'atados-test',
+            'first_name': 'Guedes', 'email': 'guedes-test@atados.com.br',
+            'username': 'guedes-test', 'password1': 'asdfgh',
+            'password2': 'asdfgh'})
+        self.assertEqual(302, response.status_code)
+        self.assertEqual(response.get('Location'),
+                         'http://testserver' + reverse('nonprofit:sign-up-complete'))
+        self.assertTemplateUsed(response, 'atados/nonprofit/activation_email.txt')
+
+    def test_sign_up_complete(self):
+        response = self.client.get(reverse('nonprofit:sign-up-complete'))
+        self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed(response, 'atados/nonprofit/sign-up-complete.html')
