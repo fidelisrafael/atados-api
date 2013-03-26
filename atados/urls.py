0957790e239c9706e4efecdcef35c6da8cc00cd5
yokomizor <mail@rogerioyokomizo.com.br>
2013-03-26 09:39:16 -0300

    fixed #3

--- atados/urls.py
+++ atados/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.views.generic.simple import direct_to_template
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
@@ -15,3 +16,8 @@ urlpatterns = patterns(
     url('', include('atados.project.urls', namespace='project')),
     url(_(r'^(?P<slug>[-\w]+)$'), slug, name='slug'),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+     'document_root': settings.MEDIA_ROOT}))
