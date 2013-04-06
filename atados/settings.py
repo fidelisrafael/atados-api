c84c02332a367b9730cee8cb7b1a0954ce8117d1
yokomizor <mail@rogerioyokomizo.com.br>
2013-04-05 23:05:31 -0300

    fixed solr query

--- atados/settings.py
+++ atados/settings.py
@@ -252,12 +252,12 @@ if all (var in os.environ for var in ('AWS_STORAGE_BUCKET_NAME',
 HAYSTACK_SITECONF = 'atados.search_indexes'
 HAYSTACK_SEARCH_ENGINE = 'atados.core.search'
 
-#if 'solr_endpoint' in AWS_EB:
-    #HAYSTACK_SOLR_URL = AWS_EB['solr_endpoint']
-#else:
-    #HAYSTACK_SOLR_URL = 'http://localhost:8983/solr'
+if 'solr_endpoint' in AWS_EB:
+    HAYSTACK_SOLR_URL = AWS_EB['solr_endpoint']
+else:
+    HAYSTACK_SOLR_URL = 'http://localhost:8983/solr'
 
-HAYSTACK_SOLR_URL = 'http://ec2-54-232-12-102.sa-east-1.compute.amazonaws.com:8080/solr'
+#HAYSTACK_SOLR_URL = 'http://ec2-54-232-12-102.sa-east-1.compute.amazonaws.com:8080/solr/'
 
 SOUTH_AUTO_FREEZE_APP = True
 
