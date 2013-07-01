f93c7e8a604aa2ffc01b1563d8160c4c24513406
yokomizor <me@ro.ger.io>
2013-07-01 00:04:55 -0300

    some layout adjustments

--- atados_legacy/views.py
+++ atados_legacy/views.py
@@ -6,14 +6,19 @@ from django.contrib.auth.models import User
 
 
 class LegacyNonprofitView(RedirectView):
-    opermanent = True
+    permanent = True
     query_string = False
     def get_redirect_url(self, pk):
         user = get_object_or_404(User, pk=pk)
         return get_object_or_404(Nonprofit, user=user).get_absolute_url()
 
 class LegacyProjectView(RedirectView):
-    opermanent = True
+    permanent = True
     query_string = False
     def get_redirect_url(self, pk):
         return get_object_or_404(Project, pk=pk).get_absolute_url()
+
+class LegacyBlogView(RedirectView):
+    permanent = True
+    url = 'http://blog.atados.com.br/%(path)s'
+    query_string = True
