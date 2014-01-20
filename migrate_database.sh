57693d96def1f7d2de99802adc6f6c7406b03ea7
Marjori Pomarole <marjoripomarole@gmail.com>
2014-01-20 19:46:02 -0200

    Updating migrate database script

--- migrate_database.sh
+++ migrate_database.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-#1 Make dump from atadosco_portal
-atados # Alias to cd into backend directory
-mysqldump -u username -p -h remote.site.com DBNAME > atadosco_portal.sql
+# Make dump from atadosco_portal
+source ~/.virtualenvs/atados/bin/activate
+cd ~/projects/atados/atados
+mysqldump -u atadosco_master -p -h atados.com.br atadosco_portal > atadosco_portal.sql # This might take a few minutes...
 
-#2 drop schema public cascade; create schema public
+# Restore the backup locally
+mysql -u root atadosco_portal -e "drop database atadosco_portal; create database atadosco_portal;"
+mysql -u root atadosco_portal < atadosco_portal.sql
+
+# Clean local new psql database
 psql -U marjoripomarole -d atados -c "drop schema public cascade; create schema public"
 
-#3 Create table and import fixtures with Django
+# Create tables and import fixtures with Django
 make install
 
 #4 migrate time
-migration # Alias to cd into migration directory
+cd ~/projects/atados/atados-migration
 make migrate
 
 # Wait and go eat something ....
+
+# make dump and restore remotely on rds
+pg_dump dbname=atados -f atados.sql
+psql -f atados.sql --host=atadosdb.cnpn2qkpvnvn.sa-east-1.rds.amazonaws.com --port=5432 --username=atadosdb --password --dbname=atadosdb
