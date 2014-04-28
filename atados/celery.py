130173bc860338acedd2804f07f20cc40a32576f
Marjori Pomarole <marjoripomarole@gmail.com>
2014-04-28 15:49:15 -0300

    fixing celery tasks

--- atados/celery.py
+++ atados/celery.py
@@ -9,13 +9,14 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atados.settings')
 
-app = Celery('atados_core')
+app = Celery('atados')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+
 @app.task(bind=True)
 def debug_task(self):
-  print('Request: {0!r}'.format(self.request))
+    print('Request: {0!r}'.format(self.request))
