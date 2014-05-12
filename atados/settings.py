8998b6aab900e20f057a19a40103ca852d5b1107
Marjori Pomarole <marjoripomarole@gmail.com>
2014-05-12 16:33:05 -0300

    fixing timedebug panel

--- atados/settings.py
+++ atados/settings.py
@@ -155,7 +155,7 @@ DEBUG_TOOLBAR_PANELS = [
     'debug_toolbar.panels.cache.CachePanel',
     'debug_toolbar.panels.headers.HeadersPanel',
     'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
     'debug_toolbar.panels.profiling.ProfilingPanel',
     'debug_toolbar.panels.redirects.RedirectsPanel',
     'debug_toolbar.panels.request.RequestPanel',
