53127ff2052d6ae64c08d1d0ccabbf727ceeec34
Marjori Pomarole <marjoripomarole@gmail.com>
2014-08-19 16:35:58 -0300

    celery app

--- atados/celery.py
+++ atados/celery.py
@@ -9,7 +9,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atados.settings')
 
-app = Celery('atados')
+app = Celery('atados_core')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
