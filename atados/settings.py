69b67441accbc65b9d063a2acdfd9af8ee455926
Marjori Pomarole <marjoripomarole@gmail.com>
2014-09-22 14:55:13 -0300

    Commenting out update and fetch for closable_results bug

--- atados/settings.py
+++ atados/settings.py
@@ -86,8 +86,8 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-  'django.middleware.cache.UpdateCacheMiddleware',
-  'django.middleware.cache.FetchFromCacheMiddleware',
+  #'django.middleware.cache.UpdateCacheMiddleware',
+  #'django.middleware.cache.FetchFromCacheMiddleware',
   'django.middleware.common.CommonMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
