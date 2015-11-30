72375ad6c7ec69419a78ae1365f1c74ca78f804b
Leonardo Arroyo <contato@leonardoarroyo.com>
2015-11-30 02:32:00 -0200

    Update user data task

--- atados_core/tasks.py
+++ atados_core/tasks.py
@@ -8,7 +8,7 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context
 
-from atados_core.models import Project
+from atados_core.models import Project, User, Address, City
 
 @task
 def send_email_to_volunteer_after_4_weeks_of_apply(volunteer_email, project_id):
@@ -40,7 +40,7 @@ def send_email_to_volunteer_3_days_before_pontual(volunteer_email, project_id):
     plaintext = get_template('email/volunteer3DaysBeforePontual.txt')
     htmly     = get_template('email/volunteer3DaysBeforePontual.html')
     d = Context({ "project_name": project.name })
-    subject, from_email, to = u'Um ato está chegando... estamos ansiosos para te ver.', 'Associação Atados <site@atados.com.br>', volunteer_email
+    subject, from_email, to = u'Um ato está chegando... estamos ansiosos para te ver.', 'Associação Atados <site@atados.com.br>', volunteer_email
     text_content = plaintext.render(d)
     html_content = htmly.render(d)
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
@@ -58,3 +58,23 @@ def send_email_to_donator_after_7_days_of_subscription(donator_email):
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
+
+@task
+def add_volunteer_address_from_csv():
+  import csv
+  with open('users.csv', 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+      try:
+        id = int(row[0])
+        u = User.objects.get(pk=row[0])
+
+        if (u.address and not u.address.city) or not u.address:
+          print row[4]
+          address = Address()
+          address.city = City.objects.get(id=row[4])
+          address.save()
+          u.address = address
+          u.save()
+      except:
+        pass
