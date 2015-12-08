7818746702fe9bed722de8ad7318fc027f6dc772
Marjori Pomarole <marjoripomarole@gmail.com>
2015-12-08 22:01:35 +0000

    Recur contributions

--- atados_core/management/commands/recur_contributions.py
+++ atados_core/management/commands/recur_contributions.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import os
+import requests
+import json
+
 from django.core.management.base import NoArgsCommand
 from atados_core.models import User, Subscription
 from dateutil.relativedelta import relativedelta
@@ -9,7 +13,7 @@ class Command(NoArgsCommand):
 
   def handle_noargs(self, **options):
     print "Recurring contributions"
-    subs = Subscription.objects.filter(recurrent=True, parent=None, active=True)
+    subs = Subscription.objects.filter(recurrent=True, parent=None, active=True, status="paid")
     for sub in subs:
       children = Subscription.objects.filter(parent=sub).order_by('-created_date')
       compare_date = sub.created_date
@@ -19,4 +23,35 @@ class Command(NoArgsCommand):
 
       compare_date = (compare_date + relativedelta(months=+1)).replace(tzinfo=None)
       if compare_date < datetime.now():
-        print "{}: {}".format(sub.name, compare_date)
+          print "[{}][{}] {}: - Expected: {}".format(sub.id, children.count(), sub.name, compare_date)
+
+          # Duplicating sub
+          new_sub = sub
+          new_sub.parent_id = sub.id
+          new_sub.pk = None
+          new_sub.recurrent = False
+          new_sub.created_date = datetime.now()
+          new_sub.status = None
+          new_sub.status_reason = None
+          new_sub.tid = None
+          new_sub.save()
+
+          # Prepare data
+          data = {'amount': int(sub.value*100), 'card_id': sub.card_id, 'api_key': os.environ.get('ATADOS_API_KEY')}
+
+          # Dump data log
+          with open("pag.txt", "a") as f:
+            f.write("[R] Data: {}\n".format(json.dumps(data)))
+
+          # Request pagar.me
+          r = requests.post('https://api.pagar.me/1/transactions', params=data)
+          resp = json.loads(r.text)
+
+          # Dump response log
+          with open("pag.txt", "a") as f:
+            f.write("[R] Response: {}\n".format(r.text.encode('utf8')))
+
+          new_sub.tid = resp.get('tid', None)
+          new_sub.status = resp.get('status', None)
+          new_sub.status_reason = resp.get('status_reason', None)
+          new_sub.save()
